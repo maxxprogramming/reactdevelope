63706c2549c1655310c40b1626537c34f964ea69
Max <sendmailtomaxx@gmail.com>
2023-06-28 16:28:18 +0200

    update

--- marvel/src/component/render/render.jsx
+++ marvel/src/component/render/render.jsx
@@ -1,32 +1,25 @@
-function RenderHtml ({data}) {
+function RenderHtml({ data }) {
+  if (!data || !data.data || !data.data.results) {
+    return <div>No data available.</div>;
+  }
 
-    if (!data || !data.data || !data.data.results) {
-        return <div>No data available.</div>;
-      }
-  
-
-   const mapData = data.data.results.map((dataM) => ({
+  const mapData = data.data.results.map((dataM) => ({
     id: dataM.id,
     name: dataM.name,
     path: dataM.thumbnail.path,
     extension: dataM.thumbnail.extension,
-  
-    
   }));
- 
-  
+
   return (
     <div>
       {mapData.map((item) => (
         <div key={item.id}>
-        
           <p>Name: {item.name}</p>
-          <img src={`${item.path}.${item.extension}`} alt={item.name} />         
+          <img src={`${item.path}.${item.extension}`} alt={item.name} />
         </div>
       ))}
     </div>
   );
-
- }
+}
 
 export default RenderHtml;
